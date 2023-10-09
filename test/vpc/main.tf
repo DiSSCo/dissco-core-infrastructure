aeed383068d7b2298c727a7b1c549e99d63a61e8
southeo <soulaine.theo@gmail.com>
2023-10-09 13:37:32 +0200

    add ip address

--- test/vpc/main.tf
+++ test/vpc/main.tf
@@ -84,6 +84,13 @@ resource "aws_security_group" "dissco-database-sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
+    description = "PostgreSQL access for Sou Home"
+    cidr_blocks = ["94.213.247.69/32"]
+  }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     description = "PostgreSQL access for Tom Home"
     cidr_blocks = ["84.24.63.197/32"]
   }
