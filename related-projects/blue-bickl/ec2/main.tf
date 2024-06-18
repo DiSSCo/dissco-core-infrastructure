8c53f794adeffdce2b74ca6d5f14f13e9ebf4cfd
southeo <soulaine.theo@gmail.com>
2024-06-18 08:26:22 +0200

    add key

--- related-projects/blue-bickl/ec2/main.tf
+++ related-projects/blue-bickl/ec2/main.tf
@@ -21,11 +21,18 @@ data "terraform_remote_state" "vpc-state" {
   }
 }
 
+resource "aws_key_pair" "key_pair" {
+  key_name = "doi_key"
+  public_key = file("./blue-bicikl.pub")
+}
+
 
 resource "aws_instance" "blue-bicikl" {
   ami                         = "ami-0a244485e2e4ffd03"
   instance_type               = "t3.small"
   associate_public_ip_address = true
+
+  key_name = aws_key_pair.key_pair.key_name
 
   subnet_id = data.terraform_remote_state.vpc-state.outputs.blue_subnets[0]
   vpc_security_group_ids = [data.terraform_remote_state.vpc-state.outputs.blue_security_group]
